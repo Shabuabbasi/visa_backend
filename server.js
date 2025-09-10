55bf115cb91664bf15370c889d2c28fda4cd728c
Salahudin <shabuabbasi456@gmail.com>
2025-09-10 11:03:33 +0500

    i

--- server.js
+++ server.js
@@ -101,4 +101,6 @@ app.use((err, req, res, next) => {
 // Start server
 // =========================
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`✅ Server running on port ${PORT}`)
+);
